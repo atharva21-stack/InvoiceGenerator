393806cb5677ea82be64955568f14845ea4d5337
atharva chandwadkar <chandwadkar28@gmail.com>
2021-10-17 15:21:25 +0530

    Updates

---
+++ precache-manifest.e4e2be9e5520f06e15662d0f8e6077ca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed7c509bb060cae0df9b9180539051e",
+    "url": "/InvoiceGenerator/index.html"
+  },
+  {
+    "revision": "51fbe2d76046e3a8f182",
+    "url": "/InvoiceGenerator/static/css/2.0d77bc6e.chunk.css"
+  },
+  {
+    "revision": "eda135653e4553244ab1",
+    "url": "/InvoiceGenerator/static/css/main.0fd7d955.chunk.css"
+  },
+  {
+    "revision": "51fbe2d76046e3a8f182",
+    "url": "/InvoiceGenerator/static/js/2.2510f0ae.chunk.js"
+  },
+  {
+    "revision": "c5f92507b76b3fbc3f8e5f5fe0c84cf3",
+    "url": "/InvoiceGenerator/static/js/2.2510f0ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda135653e4553244ab1",
+    "url": "/InvoiceGenerator/static/js/main.91cb4be5.chunk.js"
+  },
+  {
+    "revision": "310b7e9e40b4a9c47ce9",
+    "url": "/InvoiceGenerator/static/js/runtime-main.1a1717f0.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/InvoiceGenerator/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "15c01d9b81ceb6702aa92e3b5416ce50",
+    "url": "/InvoiceGenerator/static/media/remove.15c01d9b.svg"
+  },
+  {
+    "revision": "d4da359f65106dae13fb51495f035419",
+    "url": "/InvoiceGenerator/static/media/resize.d4da359f.svg"
+  }
+]);
